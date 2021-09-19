5d6c9383db6e38a759e72a89da364851bc1e52e0
Tran Hoai Thanh <hoaithanhcf123@gmail.com>
2021-09-19 13:34:04 +0700

    Commit Ex1

---
+++ 002_exercise 1/002_exercise 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _002_exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 integer numbers: ");
+            Console.Write("x = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum = " + (a + b));
+            Console.WriteLine("Subtract = " + (a - b));
+            Console.WriteLine("Multiply = " + (a * b));
+            Console.WriteLine("Divide = " + (a * 1.0 / b));
+            Console.ReadKey();
+        }
+    }
+}
