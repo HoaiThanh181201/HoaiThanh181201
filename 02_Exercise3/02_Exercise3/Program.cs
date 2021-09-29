13fe30e6bf814133cbca75f243f6ef0e57caf6c1
Tran Hoai Thanh <hoaithanhcf123@gmail.com>
2021-09-29 19:25:58 +0700

    Commit Ex3

---
+++ 02_Exercise3/02_Exercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ListAllFileInDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            ListAllFileInYourMyDocuments(@"D:\HOÀI_THANH\Exercise 3\ex3\02_Exercise3\02_Exercise3");
+            Console.WriteLine("Press Enter to continute:");
+            Console.ReadLine();
+        }
+        static void ListAllFileInYourMyDocuments(string WorkingMyDocuments)
+        {
+            string[] filePaths = Directory.GetFiles(WorkingMyDocuments);
+
+            foreach (string filePath in filePaths)
+            {
+                Console.WriteLine(filePath);
+            }
+        }
+    }
+}
